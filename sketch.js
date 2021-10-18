c74ed120d8eb0732766af14284fa2bada95d2d26
tubhyam14 <77627781+tubhyam14@users.noreply.github.com>
2021-10-18 11:24:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup() {
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
  
-  //level one
+ 
   block1 = new Block(300,275,30,40);
   block2 = new Block(330,275,30,40);
   block3 = new Block(360,275,30,40);
@@ -31,31 +31,30 @@ function setup() {
   block5 = new Block(420,275,30,40);
   block6 = new Block(450,275,30,40);
   block7 = new Block(480,275,30,40);
-  //level two
+  
   block8 = new Block(330,235,30,40);
   block9 = new Block(360,235,30,40);
   block10 = new Block(390,235,30,40);
   block11 = new Block(420,235,30,40);
   block12 = new Block(450,235,30,40);
-  //level three
+  
   block13 = new Block(360,195,30,40);
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
-  //top
+  
   block16 = new Block(390,155,30,40);
 
-  //set 2 for second stand
-  //level one
+  
   blocks1 = new Block(640,175,30,40);
   blocks2 = new Block(670,175,30,40);
   blocks3 = new Block(700,175,30,40);
   blocks4 = new Block(730,175,30,40);
   blocks5 = new Block(760,175,30,40);
-  //level two
+  
   blocks6 = new Block(670,135,30,40);
   blocks7 = new Block(700,135,30,40);
   blocks8 = new Block(730,135,30,40);
-  //top
+ 
   blocks9 = new Block(700,95,30,40);
 
  
